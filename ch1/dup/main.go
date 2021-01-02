166411be22e10c64f2167f6dfd164b05b161815b
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-01-02 14:31:07 -0500

    adds tracking of source file/s for line

--- ch1/dup/main.go
+++ ch1/dup/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	files := make(map[string]string)
 	for _, arg := range os.Args[1:] {
 		data, err := ioutil.ReadFile(arg)
 		if err != nil {
@@ -17,6 +18,12 @@ func main() {
 		}
 		for _, line := range strings.Split(string(data), "\n") {
 			counts[line]++
+			if !strings.Contains(files[line], arg) {
+				if len(files[line]) != 0 {
+					files[line] += ","
+				}
+				files[line] += arg
+			}
 		}
 	}
 
@@ -24,6 +31,6 @@ func main() {
 		if n == 0 {
 			continue
 		}
-		fmt.Printf("%d\t%s\n", n, line)
+		fmt.Printf("%d\t[%-40s]\t%s\n", n, files[line], line)
 	}
 }
