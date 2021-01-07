78393c88c618b98f9cefe6ddb5f1edd5e620220b
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-01-07 16:08:14 -0500

    serves it on 8000

--- ch3/surface/main.go
+++ ch3/surface/main.go
@@ -3,7 +3,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"net/http"
 )
 
 const (
@@ -18,26 +20,30 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay, aErr, aColor := corner(i+1, j)
-			bx, by, bErr, bColor := corner(i, j)
-			cx, cy, cErr, cColor := corner(i, j+1)
-			dx, dy, dErr, dColor := corner(i+1, j+1)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
+			"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+			"width='%d' height='%d'>", width, height)
+		for i := 0; i < cells; i++ {
+			for j := 0; j < cells; j++ {
+				ax, ay, aErr, aColor := corner(i+1, j)
+				bx, by, bErr, bColor := corner(i, j)
+				cx, cy, cErr, cColor := corner(i, j+1)
+				dx, dy, dErr, dColor := corner(i+1, j+1)
 
-			if aErr || bErr || cErr || dErr {
-				continue
+				if aErr || bErr || cErr || dErr {
+					continue
+				}
+
+				colorVal := uint32((math.Abs(aColor+bColor+cColor+dColor) / 4) * 0xffffff)
+
+				fmt.Fprintf(w, "<polygon style='fill: #%06x' points='%g,%g %g,%g %g,%g %g,%g'/>\n", colorVal, ax, ay, bx, by, cx, cy, dx, dy)
 			}
-
-			colorVal := uint32((math.Abs(aColor+bColor+cColor+dColor) / 4) * 0xffffff)
-
-			fmt.Printf("<polygon style='fill: #%06x' points='%g,%g %g,%g %g,%g %g,%g'/>\n", colorVal, ax, ay, bx, by, cx, cy, dx, dy)
 		}
-	}
-	fmt.Printf("</svg>")
+		fmt.Fprintf(w, "</svg>")
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func corner(i, j int) (float64, float64, bool, float64) {
